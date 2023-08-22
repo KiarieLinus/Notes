1e1b5a908bed894ea0bb80d59a2414448a904081
KiarieLinus <kiarielinus@gmail.com>
2023-08-22 10:25:38 +0300

    feat: EditNotes feature added

--- features/edit-note/src/main/java/com/kiarielinus/notes/edit_note/presentation/EditNoteAppBar.kt
+++ features/edit-note/src/main/java/com/kiarielinus/notes/edit_note/presentation/EditNoteAppBar.kt
@@ -1,2 +1,80 @@
 package com.kiarielinus.notes.edit_note.presentation
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Archive
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.KeyboardArrowLeft
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.kiarielinus.notes.edit_note.R
+import com.kiarielinus.notes.model.formatted
+import com.kiarielinus.notes.notes_api.NoteAction
+import java.time.LocalDateTime
+
+@Composable
+internal fun EditNoteAppBar(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    performAction: (NoteAction) -> Unit,
+) {
+    TopAppBar(
+        modifier = modifier,
+        content = {
+            IconButton(
+                onClick = navigateBack,
+                content = {
+                    Icon(
+                        imageVector = Icons.Outlined.KeyboardArrowLeft,
+                        contentDescription = null
+                    )
+                }
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            NoteAction.values().forEach { action ->
+                IconButton(
+                    onClick = {
+                        performAction(action)
+                    },
+                    content = {
+                        Icon(
+                            imageVector = when (action) {
+                                NoteAction.Archive -> Icons.Outlined.Archive
+                                NoteAction.Trash -> Icons.Outlined.Delete
+                            },
+                            contentDescription = stringResource(
+                                R.string.edit_note_perform_action,
+                                action.name
+                            )
+                        )
+                    }
+                )
+            }
+        },
+    )
+}
+
+@Composable
+internal fun EditNoteBottomBar(
+    editedAt: LocalDateTime,
+    modifier: Modifier = Modifier,
+) {
+    BottomAppBar(
+        modifier = modifier,
+        content = {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = stringResource(R.string.edit_note_edited_at, editedAt.formatted),
+                style = MaterialTheme.typography.overline
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    )
+}
--- features/edit-note/src/main/java/com/kiarielinus/notes/edit_note/presentation/EditNoteScreen.kt
+++ features/edit-note/src/main/java/com/kiarielinus/notes/edit_note/presentation/EditNoteScreen.kt
@@ -1,2 +1,126 @@
 package com.kiarielinus.notes.edit_note.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kiarielinus.notes.edit_note.R
+import com.kiarielinus.notes.notes_api.NoteAction
+import com.kiarielinus.notes.ui.theme.NotesTheme
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+
+@Composable
+internal fun EditNoteScreen(
+    title: String,
+    content: String,
+    editedAt: LocalDateTime,
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    performAction: (NoteAction?) -> Unit,
+    onTitleChanged: (String) -> Unit,
+    onContentChanged: (String) -> Unit,
+) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutine = rememberCoroutineScope()
+    val actionMessage = stringResource(R.string.edit_note_action_performed)
+    val undoMessage = stringResource(R.string.edit_note_undo)
+
+    Scaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        topBar = {
+            EditNoteAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                navigateBack = navigateBack,
+                performAction = {
+                    coroutine.launch {
+                        val result = scaffoldState.snackbarHostState.showSnackbar(
+                            message = "$actionMessage ${it}d",
+                            actionLabel = undoMessage
+                        )
+                        when (result) {
+                            SnackbarResult.Dismissed -> {}
+                            SnackbarResult.ActionPerformed -> {
+                                performAction(null)
+                            }
+                        }
+                    }
+                    performAction(it)
+                }
+            )
+        },
+        content = { padding ->
+            Column(
+                content = {
+                    OutlinedTextField(
+                        value = title,
+                        onValueChange = onTitleChanged,
+                        textStyle = MaterialTheme.typography.h6,
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            backgroundColor = Color.Transparent,
+                            focusedBorderColor = Color.Transparent,
+                            unfocusedBorderColor = Color.Transparent
+                        ),
+                        placeholder = {
+                            Text(
+                                text = stringResource(R.string.edit_note_title),
+                                style = MaterialTheme.typography.h6
+                            )
+                        }
+                    )
+                    Divider()
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding),
+                        value = content,
+                        onValueChange = onContentChanged,
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            backgroundColor = Color.Transparent,
+                            focusedBorderColor = Color.Transparent,
+                            unfocusedBorderColor = Color.Transparent
+                        ),
+                        placeholder = {
+                            Text(text = stringResource(R.string.edit_note_note))
+                        }
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            EditNoteBottomBar(
+                modifier = Modifier.fillMaxWidth(),
+                editedAt = editedAt,
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun EditNoteScreenPreview() = NotesTheme {
+    EditNoteScreen(
+        title = "Stub note",
+        content = "111111111111111",
+        editedAt = LocalDateTime.now(),
+        navigateBack = {},
+        onContentChanged = {},
+        onTitleChanged = {},
+        performAction = { }
+    )
+}
